6653cf940885daedebbdabf5a19c139fbdcf0da0
Freddy Caceres <freddy@freddygraphics.com>
2026-01-09 21:42:42 -0500

    Fix middleware invocation error (safe root redirect)

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -1,13 +1,9 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  const url = request.nextUrl;
-
-  if (url.pathname === "/") {
+  if (request.nextUrl.pathname === "/") {
     return NextResponse.redirect(new URL("/login", request.url));
   }
-
-  return NextResponse.next();
 }
 
 export const config = {
